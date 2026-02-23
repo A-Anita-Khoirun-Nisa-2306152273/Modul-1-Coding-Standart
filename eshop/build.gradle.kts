981e1893d60f05f62610ebaef2798d97a450b526
anitakny <antkhrnns9@gmail.com>
2026-02-23 10:18:43 +0700

    Add JaCoCo configuration and test setup

--- eshop/build.gradle.kts
+++ eshop/build.gradle.kts
@@ -5,6 +5,7 @@ val junitJupiterVersion = "5.9.1"
 
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -64,4 +65,23 @@ tasks.register<Test>("functionalTest") {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    // Exclude functional tests
+    exclude("**/*FunctionalTest*")
+
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        csv.required.set(false)
+    }
 }
